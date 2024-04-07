d1d0503055b441278b3dfe2c01e9de212e4ada23
Kanan Farzali <kanan.farzali@gmail.com>
2024-04-07 16:16:50 +0200

    adding modal functionality using rect-modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
 "use client";
 
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { signIn, useSession, signOut } from "next-auth/react";
+import Modal from "react-modal";
+import { IoMdAddCircleOutline } from "react-icons/io";
+import { HiCamera } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Header = () => {
   const { data: session } = useSession();
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="shadow-sm border-b sticky top-0 bg-white z-10 p-3">
@@ -27,12 +33,18 @@ const Header = () => {
         />
         {/* Menu Items */}
         {session ? (
-          <img
-            src={session?.user?.image}
-            alt={session?.user?.name}
-            className="h-10 w-10 rounded-full cursor-pointer"
-            onClick={signOut}
-          />
+          <div className="flex gap-2 items-center">
+            <IoMdAddCircleOutline
+              onClick={() => setIsOpen(true)}
+              className="text-2xl cursor-pointer transform hover:scale-125 transition duration-300 hover:text-red-600"
+            />
+            <img
+              src={session?.user?.image}
+              alt={session?.user?.name}
+              className="h-10 w-10 rounded-full cursor-pointer"
+              onClick={signOut}
+            />
+          </div>
         ) : (
           <button
             onClick={() => signIn()}
@@ -42,6 +54,40 @@ const Header = () => {
           </button>
         )}
       </div>
+      {isOpen && (
+        <Modal
+          isOpen={isOpen}
+          className="max-w-lg w-[90%] p-6 absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md"
+          onRequestClose={() => setIsOpen(false)}
+          ariaHideApp={false}
+        >
+          <div className="flex flex-col justify-center items-center h-[100%]">
+            <HiCamera className="text-5xl text-gray-400 cursor-pointer" />
+          </div>
+          <input
+            type="text"
+            maxLength="150px"
+            placeholder="Title"
+            className="m-4 border-none text-center w-full focus:ring-0 outline-none"
+          />
+          <input
+            type="text"
+            maxLength="150px"
+            placeholder="Description"
+            className="m-4 border-none text-center w-full focus:ring-0 outline-none"
+          />
+          <button
+            disabled
+            className="w-full bg-red-600 text-white p-2 shadow-md rounded-lg hover:brightness-105 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:brightness-100"
+          >
+            Create a task
+          </button>
+          <AiOutlineClose
+            className="cursor-pointer absolute top-2 right-2 hover:text-r-ed-600 transition duration-300"
+            onClick={() => setIsOpen(false)}
+          />
+        </Modal>
+      )}
     </div>
   );
 };
