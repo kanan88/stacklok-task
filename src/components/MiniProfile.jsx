4ddc3dc5b5295926da1ab00f1bb66c44f7428ace
Kanan Farzali <kanan.farzali@gmail.com>
2024-04-08 00:34:51 +0200

    completing mini profile component

--- src/components/MiniProfile.jsx
+++ src/components/MiniProfile.jsx
@@ -1,5 +1,36 @@
-import React from "react";
+/* eslint-disable @next/next/no-img-element */
+"use client";
+
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function MiniProfile() {
-  return <div>MiniProfile</div>;
+  const { data: session } = useSession();
+  return (
+    <div className="flex items-center justify-between mt-14 scroll-ml-10">
+      <img
+        src={session?.user?.image || "https://stacklok.com/img/favicon.svg?v2"}
+        alt="user profile pic"
+        className="w-16 h-16 rounded-full border p-[2px]"
+      />
+      <div className="flex-1 ml-4">
+        <h2 className="font-bold">{session?.user?.name}</h2>
+        <h3 className="text-sm text-gray-400">Welcome to Dashboard!</h3>
+      </div>
+      {session ? (
+        <button
+          onClick={signOut}
+          className="text-blue-500 text-sm font-semibold"
+        >
+          Sign Out
+        </button>
+      ) : (
+        <button
+          onClick={signIn}
+          className="text-blue-500 text-sm font-semibold"
+        >
+          Sign In
+        </button>
+      )}
+    </div>
+  );
 }
