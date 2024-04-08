eed3af3e52c9b3fa6bb4d36149d488d17d9d0845
Kanan Farzali <kanan.farzali@gmail.com>
2024-04-08 13:46:23 +0200

    adding forcedynamic to have real time experience

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,13 @@
 import Feed from "@/components/Feed";
 
+export const dynamice = "force-dynamic";
+
 const Home = () => {
-  return <Feed />;
+  return (
+    <>
+      <Feed />
+    </>
+  );
 };
 
 export default Home;
